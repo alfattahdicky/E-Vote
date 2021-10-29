6f651cf77a8ce969e17b2b5e6cd843fe404c86d3
fiqamd <taufiqahmadi1122@gmail.com>
2021-10-29 23:07:35 +0700

    taufiq

--- vite.config.js
+++ vite.config.js
@@ -8,10 +8,12 @@ module.exports = defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'src/index.html'),
+        landing: resolve(__dirname, 'src/pages/landing.html'),
         infopaslon: resolve(__dirname, 'src/pages/infopaslon.html'),
+        paslon: resolve(__dirname, 'src/pages/paslon.html'),
         adminhome: resolve(__dirname, 'src/pages/admin/home.html'),
         admindata: resolve(__dirname, 'src/pages/admin/data.html'),
-        formpaslon: resolve(__dirname, 'src/pages/admin/form.html')
+        formpaslon: resolve(__dirname, 'src/pages/admin/form.html'),
       }
     }
   }
